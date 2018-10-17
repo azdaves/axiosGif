e1579679523f5114008a04230f5a917c47460084
Dave Schilling <ds@daves-mbp.wusd.local>
2018-10-17 13:21:51 -0700

    Start with create-react-app

---
+++ src/Components/NoGifs.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NoGifs = props => (
+  <li className="no-gifs">
+    <i className="material-icons icon-gif">sentiment_very_dissatisfied</i>
+    <h3>
+      Sorry, no GIFs match your search. Try again in Dave's Famous Gif Search
+      Bar above.
+    </h3>
+  </li>
+);
+
+export default NoGifs;
